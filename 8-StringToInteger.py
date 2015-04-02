c750ffcff997efd2964cc4292f0d18baed9da456
Xin Han <hh235206661@gmail.com>
2015-04-02 21:35:26 +0200

    Q8

---
+++ 8-StringToInteger.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @return an integer
+    def atoi(self,str):
+        str = str.strip()
+        str = re.findall('(^[\+\-0]*\d+\D*',str)
+
+        try:
+            result = int(''.join(str))
+            result = int(str)
+            INT_MAX = 2147483647
+            INT_MIN = -2147483648
+            if result > INT_MAX:
+                return INT_MAX
+            elif result < INT_MIN:
+                return INT_MIN
+            else:
+                return result
+        except:
+            return 0
